09207173c215f411af94f5d419870f152d76de81
Alaa <toalaa9@gmail.com>
2024-05-30 22:55:48 +0200

    await promise added to fix pro try#1

--- app/utils/mail.js
+++ app/utils/mail.js
@@ -2,34 +2,33 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, html) => {
-	// create reusable transporter object using the default SMTP transport
+	const transporter = nodemailer.createTransport({
+		host: "smtp.zoho.eu",
+		port: 465,
+		secure: true,
+		auth: {
+			user: process.env.ZOHO_EMAIL,
+			pass: process.env.ZOHO_PASSWORD,
+		},
+	});
+	let mailOptions = {
+		from: process.env.ZOHO_EMAIL,
+		to: email,
+		subject: subject,
+		html: html,
+	};
 	await new Promise((resolve, reject) => {
-		const transporter = nodemailer.createTransport({
-			host: "smtp.zoho.eu",
-			port: 465,
-			secure: true,
-			auth: {
-				user: process.env.ZOHO_EMAIL,
-				pass: process.env.ZOHO_PASSWORD,
-			},
-		});
-		let mailOptions = {
-			from: process.env.ZOHO_EMAIL,
-			to: email,
-			subject: subject,
-			html: html,
-		};
-		console.log(mailOptions);
 		transporter.sendMail(mailOptions, (error, info) => {
 			if (error) {
 				console.log(error);
-				reject(err);			}
-			else
+				reject(error)
+			}
+			else{
+				console.log('Message sent: %s', info.messageId);
 				resolve(info)
-			console.log('Message sent: %s', info.messageId);
+			}
 		});
-
-	});
-}
+	})
+};
 
 module.exports = sendEmail;
